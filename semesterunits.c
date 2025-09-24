2b708e8915ca42f73e6ec5e86d1395c6605dddab
catekatoto23-netizen <catekatoto23@gmail.com>
2025-09-24 20:31:55 +0300

    Add files via upload

---
+++ semesterunits.c
@@ -0,0 +1,21 @@
+//units in year one semester one
+/*
+Name:Catherine Ndung'u
+Reg No:PA106/G/28730/25
+Description: To displayunits in software engineering
+
+*/
+#include<stdio.h>//
+int main()
+{
+	printf("          -SEMESTER ONE UNITS-      \n");
+printf("\t 1.mathemetics for science \n");
+printf("\t 2.Discrete mathematics \n");
+printf("\t 3.communication skills \n");
+printf("\t 4.fundamentals to ICT \n");
+printf("\t 5.computer system and organaization \n");
+printf("\t 6.introduction to programing and promblem solving \n");
+printf("\t 7.fundamental of system analysis and design \n");
+
+return 0;
+}
